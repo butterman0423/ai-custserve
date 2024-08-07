7a811f308d32d3010c62add878ece9cbc18a91cc
Nathaniel Escaro <butterman423@gmail.com>
2024-08-07 01:03:26 -0400

    Scroll on record overflow - Refactored event callbacks in main page

--- app/pages/support/page.tsx
+++ app/pages/support/page.tsx
@@ -7,25 +7,30 @@ import InputField from "@/components/InputField";
 
 export default function Support() {
     const [messages, setMessages] = useState<string[]>([]);
+
+    const cards = messages.map((txt, idx) => {
+        return (
+            <TextCard 
+                className='bg-primary'
+                key={idx} name='Sample' 
+                time={0} text={txt} />
+        );
+    });
+
+    async function sendPrompt(txt: string) {
+        const searchParams = new URLSearchParams({
+            prompt: txt
+        })
+        const res = await fetch(`/api/talk?${searchParams.toString()}`);
+        setMessages([...messages, txt, await res.text()]);
+    }
+
     return (
         <div className="container mx-auto h-screen bg-secondary">
             <Record className="w-100 h-4/6 mb-4">
-                { messages.map((txt, idx) => {
-                    return (
-                        <TextCard 
-                            className='bg-primary min-h-32'
-                            key={idx} name='Sample' 
-                            time={0} text={txt} />
-                    );
-                })}
+                { cards }
             </Record>
-            <InputField className="w-100 h-1/6 p-6" onClick={async (txt) => {
-                const searchParams = new URLSearchParams({
-                    prompt: txt
-                })
-                const res = await fetch(`/api/talk?${searchParams.toString()}`);
-                setMessages([...messages, txt, await res.text()]);
-            }}/>
+            <InputField className="w-100 h-1/6 p-6" onClick={sendPrompt}/>
         </div>
     );
 }
