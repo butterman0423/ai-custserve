dd1f4aad4406b2609dcb3594e05252ec226cc1c9
Nathaniel Escaro <butterman423@gmail.com>
2024-08-08 15:12:33 -0400

    Name display on login

--- app/support/layout.tsx
+++ app/support/layout.tsx
@@ -17,6 +17,9 @@ export default async function RootLayout({
         <>
             <nav>
                 <SignOutButton/>
+                <div className="float-end">
+                    { session?.user?.name }
+                </div>
             </nav>
             <main>{ children }</main>
         </>
