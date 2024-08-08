962ca30709eca8af24956f7b388a8a0c460f91a8
Nathaniel Escaro <butterman423@gmail.com>
2024-08-08 12:01:00 -0400

    Google & Github Providers

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
+import github from "next-auth/providers/github"
+import google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [github, google],
 })
